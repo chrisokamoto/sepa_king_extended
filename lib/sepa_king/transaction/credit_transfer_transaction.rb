4d616b3c6f3157c1b99ea60ec3ce20ecc680508d
Chris Okamoto <christiane.okamoto@gapfish.com>
2018-04-10 10:57:09 +0200

    don't set service_level in xml file if attribute is blank

--- lib/sepa_king/transaction/credit_transfer_transaction.rb
+++ lib/sepa_king/transaction/credit_transfer_transaction.rb
@@ -3,19 +3,23 @@ module SEPA
   class CreditTransferTransaction < Transaction
     attr_accessor :service_level
 
-    validates_inclusion_of :service_level, :in => %w(SEPA URGP)
+    validates_inclusion_of :service_level, :in => ['SEPA', 'URGP', '']
 
     validate { |t| t.validate_requested_date_after(Date.today) }
 
     def initialize(attributes = {})
       super
-      self.service_level ||= 'SEPA'
+      if self.currency == 'EUR'
+        self.service_level ||= 'SEPA'
+      end
     end
 
     def schema_compatible?(schema_name)
       case schema_name
       when PAIN_001_001_03
-        self.service_level == 'SEPA'
+        if self.currency == 'EUR'
+          self.service_level == 'SEPA'
+        end
       when PAIN_001_002_03
         self.bic.present? && self.service_level == 'SEPA' && self.currency == 'EUR'
       when PAIN_001_003_03
